c2ced362571ac52a8c26335adde3f7a66e14400b
Jeffrey (Siothrún) Sardina <jeffrey.sardina@gmail.com>
2021-01-22 23:36:58 +0000

    more basic stuff

--- index.php
+++ index.php
@@ -16,19 +16,53 @@
                 writing-mode: vertical-lr;
             }
 
+            h1
+            {
+                text-align: center;
+                font-size: 2.5em;
+            }
+
+            .centerbox
+            {
+                margin: auto;
+                width: 60%;
+                padding: 10px;
+                font-size: 1.5em;
+            }
+
         
         </style>
     </head>
 
     <body>
-        <p class="ccaizhdi">hame
-            <br>gaew ma sitaiw</br>
-        </p>
+        <h1>Welcome to the site of the Ccaizhdi language!</h1>
 
-        <textarea class="ccaizhdi" rows="50" cols="20">
-            hame
-            <br>gaew ma sitaiw</br>
-        </textarea>
+        <p class='centerbox'>
+            This site will contain lore, learning resources, and a text editor for my favorite fantasy langauge, Ccaizhdi (aka Fantask Klingon). It's still in development, so please be aware that features will be being added over time.
+            <br><br>
+            Have fun learning Fantasy Klingon!
+            <br>
+            --Jeffrey (Siothrún) Sardina
+
+            <br><br>
+            Links:
+                <a href='texteditor.php'>Type Ccaizhdi</a>
+            <ul>
+
+            In case you are curious, here is a sample of the language:
+            Meaning: "Drown demons, poinson snakes, and trun Deons into Suns!"
+            Romanized: "Roppaestosh shae, zhoxaesh zhox, zai shosh ri aefshosh neft!"
+
+            <p class='ccaizhdi'>
+                Ropaestow wae
+                <br>
+                yoxaew uox
+                <br>
+                zai wow ri aefwow neft!
+            </p>
+
+            Note: Ccaizhdi is written top-to-bottom, left-tio-right.
+        </p>
 
     </body>
 </html>
---
+++ texteditor.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style> 
+            @font-face
+            {
+                font-family:"ccaizhdi";
+                src: url("ccaizhdi_rotated.woff") format("woff");
+                font-weight:normal;
+                font-style:normal;
+            }
+            
+            .ccaizhdi 
+            {
+                font-family: 'ccaizhdi';
+                writing-mode: vertical-lr;
+            }
+
+            h1
+            {
+                text-align: center;
+                font-size: 2.5em;
+            }
+
+            .centerbox, textarea
+            {
+                margin: auto;
+                width: 60%;
+                padding: 10px;
+                font-size: 1.5em;
+            }
+
+            .table
+            {
+                width: 100%;
+            }
+
+        
+        </style>
+    </head>
+
+    <body>
+        <h1>Welcome to the site of the Ccaizhdi language!</h1>
+
+        <div class='centerbox'>
+            <a href='index.php'>Home</a>
+            <br>
+            Go ahead and use the text box below to type in the Ccaizhdi script. It is still being developed, so only lower-case characters will work. Numbers and warcase (the Ccaizhdi 'upper case' of sorts) will be added later.
+            
+            Note that the font used is generally easy on the QWERTY keyboard. However, some bindings are not intuitive, so I have a list below:
+            <br>
+            <table>
+            <tr>
+                <th>Key----</th>
+                <th>Sound english /IPA/----</th>
+                <th>Letter Name----</th>
+            </tr>
+            <tr>
+                <td>a</td>
+                <td>a /a/</td>
+                <td>a</td>
+            </tr>
+            <tr>
+                <td>b</td>
+                <td>b /b/</td>
+                <td>bif</td>
+            </tr>
+            <tr>
+                <td>c</td>
+                <td>k! or k /k'/ or /k/</td>
+                <td>Ccimhai</td>
+            </tr>
+            <tr>
+                <td>d</td>
+                <td>d /d/</td>
+                <td>dezhaix</td>
+            </tr>
+            <tr>
+                <td>e</td>
+                <td>ay /e/</td>
+                <td>e</td>
+            </tr>
+            <tr>
+                <td>f</td>
+                <td>f /f/</td>
+                <td>fae</td>
+            </tr>
+            <tr>
+                <td>g</td>
+                <td>g /g/</td>
+                <td>gae</td>
+            </tr>
+            <tr>
+                <td>h</td>
+                <td>h /h/</td>
+                <td>hob</td>
+        </tr>
+            <tr>
+                <td>i</td>
+                <td>ee /i/</td>
+                <td>i</td>
+            </tr>
+            <tr>
+                <td>m</td>
+                <td>m /m/</td>
+                <td>mazh</td>
+            </tr>
+            <tr>
+                <td>n</td>
+                <td>n /n/</td>
+                <td>nuf</td>
+            </tr>
+            <tr>
+                <td>ñ or j</td>
+                <td>ny /ɲ/</td>
+                <td>ñarazh</td>
+            </tr>
+            <tr>
+                <td>o</td>
+                <td>oh /o/</td>
+                <td>o</td>
+            </tr>
+            <tr>
+                <td>p</td>
+                <td>p! or p /p'/ or /p/</td>
+                <td>ppox</td>
+            </tr>
+            <tr>
+                <td>r</td>
+                <td>gutteral r /ʁ/</td>
+                <td>hob</td>
+            </tr>
+            <tr>
+                <td>s</td>
+                <td>s /s/</td>
+                <td>saif</td>
+            </tr>
+            <tr>
+                <td>w</td>
+                <td>sh /ʃ/</td>
+                <td>sho</td>
+            </tr>
+            <tr>
+                <td>t</td>
+                <td>t or t! /t'/ or /t/</td>
+                <td>saif</td>
+            </tr>
+            <tr>
+                <td>u</td>
+                <td>oo /u/</td>
+                <td>u</td>
+            </tr>
+            <tr>
+                <td>v</td>
+                <td>v /v/</td>
+                <td>vaem</td>
+            </tr>
+            <tr>
+                <td>x</td>
+                <td>ch as in loch /x/</td>
+                <td>xu</td>
+            </tr>
+
+            <tr>
+                <td>z</td>
+                <td>z /z/</td>
+                <td>ze</td>
+            </tr>
+            <tr>
+                <td>y</td>
+                <td>zh (voiced sh) /ʒ/</td>
+                <td>zhi</td>
+            </tr>
+            </table>
+        </div>
+
+        <textarea class="ccaizhdi" rows="50" cols="20"></textarea>
+
+    </body>
+</html>
